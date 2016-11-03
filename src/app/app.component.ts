0208604670bc63c9116d62ec76e629e092405f42
r-vijay-r <503r.vijay@gmail.com>
2016-11-03 12:53:05 +0530

    working push notification without timeout option and icon option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { PushNotificationsService } from 'angular2-notifications';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(private _pushNotifications: PushNotificationsService){
+  	_pushNotifications.requestPermission();
+  	_pushNotifications.create('Welocome!', {body: 'This is made for testing push notifiactions in angular2'}).subscribe(
+            res => console.log(res),
+            err => console.log(err)
+        )
+  }
+  send(title:string,body:string){
+  	this._pushNotifications.create(title,{body: body}).subscribe(
+            res => console.log(res),
+            err => console.log(err)
+        );
+  }
 }
