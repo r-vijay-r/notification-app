966e3f2fe98e07db53934f2f2bcfac4366429361
r-vijay-r <503r.vijay@gmail.com>
2016-11-08 12:10:45 +0530

    preloaded sound and play on notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,19 @@ import { PushNotificationsService } from 'angular2-notifications';
 })
 export class AppComponent {
   title = 'app works!';
+  _audio = new Audio();
   constructor(private _pushNotifications: PushNotificationsService){
-  	_pushNotifications.requestPermission();
-  	_pushNotifications.create('Welocome!', {body: 'This is made for testing push notifiactions in angular2'}).subscribe(
+    _pushNotifications.requestPermission();
+    _pushNotifications.create('Welocome!', {body: 'This is made for testing push notifiactions in angular2',icon:'../favicon.ico'}).subscribe(
             res => console.log(res),
             err => console.log(err)
         )
+    this._audio.src = "http://soundbible.com/grab.php?id=2084&type=wav";
+    this._audio.load();
   }
   send(title:string,body:string){
-  	var audio = new Audio();
-	audio.src = "http://soundbible.com/grab.php?id=2084&type=wav";
-	audio.load();
   	this._pushNotifications.create(title,{body: body,icon:'../favicon.ico'}).subscribe(
-            res => audio.play(),
+            res => this._audio.play(),
             err => console.log(err)
         );
   }
